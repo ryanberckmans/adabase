1277d355205f4551db0feac0d8ea95a176990be2
Justin Giancola <justin.giancola@gmail.com>
2010-06-13 18:06:54 -0400

    initial commit; empty rails project

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta4'
+
+# data slinging
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'right_aws'
+
+# don't bother loading these into memory by default
+group :deploy do
+  gem 'unicorn'
+  gem 'capistrano'
+end
+
+# test libs
+group :test do
+  gem 'rspec'
+  gem 'rspec-rails', '>= 2.0.0.beta.10'
+  gem 'autotest'
+  gem 'autotest-rails'
+  gem 'webrat'
+  gem 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '480f7ac944e08d501114e75618e392fd83213c971aa331961e3d98b96a25d5725839724ee0106a85b012aaa0af74f1a42f9e13234cfd23850d605f9212be95d4'
