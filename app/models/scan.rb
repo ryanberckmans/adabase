bae7c2c6f4fa3bf81ae2f2fc38ad2b52739e90c3
Ryan Berckmans <ryan.berckmans@gmail.com>
2010-11-23 16:23:50 -0500

    scan model validates_presence domain, path

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -5,6 +5,8 @@ class Scan < ActiveRecord::Base
 
   accepts_nested_attributes_for :ads, :screenshot
 
+  validates_presence_of :domain, :path
+
   def self.from_open_struct(os)
     attrs = os.marshal_dump
     ads = (attrs.delete(:ads) || []).map(&:marshal_dump)
